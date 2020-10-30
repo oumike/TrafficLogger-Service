cad6dc2200a277cda6db5c46a321866a9bd12dd5
Michael A. Cojocari <michael.cojocari@gmail.com>
2020-10-30 13:34:33 -0400

    Adding stuff.

--- app.py
+++ app.py
@@ -4,3 +4,18 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+
+traffic_logs = {}
+
+class TrafficLog(Resource):
+    def get(self, traffic_log_id):
+        return {traffic_log_id: traffic_logs[traffic_log_id]}
+
+    def put(self, traffic_log_id):
+        traffic_logs[traffic_log_id] = request.form['data']
+        return {traffic_log_id: traffic_logs[traffic_log_id]}
+
+api.add_resource(TrafficLog, '/<string:traffic_log_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
